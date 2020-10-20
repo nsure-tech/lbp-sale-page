2930cf964adef6ab011945278a5cd5ed47d30eb5
FangYu199 <talentyupush@163.com>
2020-10-20 17:21:37 +0800

    inde.html

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
             }
         }
     },
-    publicPath: process.env.NODE_ENV === 'production'
-        ? './'
-        : './'
+    // publicPath: process.env.NODE_ENV === 'production'
+    //     ? '/'
+    //     : '/'
 };
