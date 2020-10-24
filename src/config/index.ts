f559954b38fa5670e7cd699d930d4711292bd9a9
Leafan.Chan <leafan@qq.com>
2020-10-24 19:59:03 +0800

    update token

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 // token配置
 const token = {
-    BPool: '0x27fdccd1a9ee46b46b3d4547cbf4c57bc10c0508',
+    BPool: '0x4C709bE5dfcE030a7719Af9eC8e472D57803e569',
 };
 
 export const Config = {
     bPoolTokenIn: process.env.BPOOL_TOKENIN || '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    bPoolTokenOut: process.env.BPOOL_TOKENOUT || '0xe22e5Acede7391E41bcbb2af7b2D12787537470b',
+    bPoolTokenOut: process.env.BPOOL_TOKENOUT || '0xDaDDf3D778b9188e731d14F8F2207FF6eA03FBE0',
 };
 
 export const bpoolTOken = token.BPool;
